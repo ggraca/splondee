619935fc58fdc733b5572cf28604c1477770f8f0
Guilherme Graca <ggracase@gmail.com>
2014-04-26 19:18:50 +0100

    Added some pipes and started the flow detection algorithm

--- Map.js
+++ Map.js
@@ -12,10 +12,11 @@ function Map(x, y, width, height){
 		for(var i = 0; i < this.MAX_Y; i++){
 			this.pos[i] = new Array(this.MAX_X);
 			for(var j = 0; j < this.MAX_X; j++){
-				this.pos[i][j] = new PipeVertical(x + j*50, y + i*50, 50, 50);
+				this.pos[i][j] = new PipeVertical(x + j*50, y + i*50, 50, 50, i, j);
 			}
 		}
-		this.pos[0][0] = new PipeHorizontal(x + 0, y + 0, 50, 50);
+		this.pos[0][0] = new PipeHorizontal(x + 0, y + 0, 50, 50, 0, 0);
+		this.pos[3][3] = new PipeCurve(x + 3*50, y + 3*50, 50, 50, 3, 3);
 	}
 
 	this.draw = function(){
@@ -30,16 +31,38 @@ function Map(x, y, width, height){
 		for(var i = 0; i < this.MAX_Y; i++){
 			for(var j = 0; j < this.MAX_X; j++){
 				if(this.pos[i][j].handleClick(mouse)){
-					if(this.selectedPipe == null)
-						this.selectedPipe = this.pos[i][j];
-					else{
-						if(this.selectedPipe != this.pos[i][j])
-							this.pos[i][j].swap(this.selectedPipe);
-						this.selectedPipe = null;
-					}
+
+					this.flow(this.pos[i][j], null);
+
+					// if(this.selectedPipe == null)
+					// 	this.selectedPipe = this.pos[i][j];
+					// else{
+					// 	if(this.selectedPipe != this.pos[i][j])
+					// 		this.pos[i][j].swap(this.selectedPipe);
+					// 	this.selectedPipe = null;
+					// }
 					return;
 				}
 			}
+		}
+	}
+
+	this.flow = function(p, old){
+		if(p.marked)
+			return;
+
+		p.mark();
+		var l = p.dest();
+
+		for(var a = 0; a < l.length; a++){
+			var x = l[a][0] + p.j;
+			var y = l[a][1] + p.i;
+
+
+			if(x < 0 || x >= this.MAX_X || y < 0 || y >= this.MAX_Y)
+				continue;
+
+			this.flow(this.pos[y][x], p);
 		}
 	}
 }
--- Pipes.js
+++ Pipes.js
@@ -1,12 +1,13 @@
-function Pipe(x, y, width, height){
+function Pipe(x, y, width, height, i, j){
 	GameObject.apply(this, [x, y, width, height]);
-	this.sprite = new Image();
-	this.sprite.src = "res/img/PALHINHAS6.png";	
 
-	this.left = null;
-	this.right = null;
-	this.up = null;
-	this.down = null;
+	this.i = i;
+	this.j = j;
+	this.marked = false
+
+	this.sprite = new Image();
+
+	this.dest = [];
 
 	this.draw = function(){
 		canvas.drawImage(this.sprite, this.x, this.y, this.width, this.height);
@@ -25,13 +26,45 @@ function Pipe(x, y, width, height){
 		p.copy(this);
 		this.copy(temp);
 	}
+
+	this.flow = function(p){
+		for(var i = 0; i < dest.size; i++){
+			if(dest[i] != p){
+				dest[i].flow(this);
+			}
+		}
+	}
+
+	this.mark = function(){
+		this.sprite.src = "res/img/PALHINHAS4.png";
+		this.marked = true;
+	}
+
 }
 
-function PipeVertical(x, y, width, height){
-	Pipe.apply(this, [x, y, width, height]);
-	this.sprite.src = "res/img/PALHINHAS6.png";	
+function PipeVertical(x, y, width, height, i, j){
+	Pipe.apply(this, [x, y, width, height, i, j]);
+	this.sprite.src = "res/img/PALHINHAS6.png";
+
+	this.dest = function(){
+		return [[0, -1], [0, 1]];
+	}
 }
-function PipeHorizontal(x, y, width, height){
-	Pipe.apply(this, [x, y, width, height]);
+
+function PipeHorizontal(x, y, width, height, i, j){
+	Pipe.apply(this, [x, y, width, height, i, j]);
 	this.sprite.src = "res/img/PALHINHAS7.png";	
+
+	this.dest = function(){
+		return [[-1, 0], [1, 0]];
+	}
+}
+
+function PipeCurve(x, y, width, height, i, j){
+	Pipe.apply(this, [x, y, width, height, i, j]);
+	this.sprite.src = "res/img/PALHINHAS.png";
+
+	this.dest = function(){
+		return [[0, -1], [-1, 0]];
+	}
 }
