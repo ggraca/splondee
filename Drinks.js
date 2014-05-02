bf73ad2a051b4194f0a7ace49f1f23071774e430
Mário Balça <mario.balca@gmail.com>
2014-05-03 00:01:19 +0100

    RESET A ESTA MERDA TODA

--- Drinks.js
+++
@@ -1,9 +0,0 @@
-function Drink(x, y, j){
-	GameObject.apply(this, [x, y, 100, 125]);
-	this.sprite = new Image();
-	this.sprite.src = "res/img/Drink.png";
-
-	this.draw = function(){
-		canvas.drawImage(this.sprite, this.x, this.y, this.width, this.height);
-	}
-}
