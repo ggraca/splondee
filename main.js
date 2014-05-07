3db6a635c914a9490debcf220fa918bc8ef1f6c4
Mário Balça <mario.balca@gmail.com>
2014-05-07 10:40:30 +0100

    Added UI.js, who controls all the user interface experience

--- main.js
+++ main.js
@@ -1,14 +1,6 @@
 var stage = new createjs.Stage("myCanvas");
 
-var circle = new createjs.Shape();
-circle.graphics.beginFill("red").drawCircle(0, 0, 50);
-circle.x = 100;
-circle.y = 100;
-stage.addChild(circle);
-
-createjs.Ticker.addEventListener("tick", tick);
-function tick() { console.log("TICK!!!"); }
-
+mainMenu();
 
 stage.update();
 
