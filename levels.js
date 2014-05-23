c28bc9fe4bc3cffe4ddfe63267bae864bb17b285
Guilherme Graca <ggracase@gmail.com>
2014-05-23 04:47:11 +0100

    replaced level9 with the shuffled one

--- levels.js
+++ levels.js
@@ -34,28 +34,28 @@ var level3 = {
 	receiver: ["wine", null, null, "wine", null, null, null, "wine", null, null, null]
 };
 
-// var level9 = {
-// 	drinks: ["liquor", "wine", null, "soda", "tequila"],
-// 	pipes: [
-// 		[["rect", 0], ["rect", 1], ["curve", 2], ["curve", 2], ["curve", 3], ["mixer", 0], ["cross", 0], ["rect", 0], ["rect", 1], ["rect", 0], ["segundo", 1]],
-// 		[["rect", 0], ["cross", 0], ["bridge", 0], ["curve", 1], ["mixer", 0], ["curve", 3], ["rect", 1], ["segundo", 0], ["curve", 1], ["curve", 1], ["rect", 0]],
-// 		[["rect", 1], ["rect", 0], ["rect", 0], ["rect", 0], ["rect", 0], ["curve", 3], ["curve", 1], ["curve", 0], ["bridge", 0], ["rect", 0], ["curve", 2]],
-// 		[["rect", 0], ["curve", 0], ["curve", 0], ["cross", 0], ["segundo", 0], ["curve", 0], ["mixer", 0], ["rect", 0], ["rect", 0], ["rect", 0], ["rect", 0]],
-// 		[["segundo", 0], ["rect", 0], ["segundo", 0], ["rect", 0], ["segundo", 0], ["curve", 1], ["segundo", 0], ["bridge", 0], ["rect", 0], ["rect", 0], ["curve", 1]]
-// 	],
-// 	receiver: ["hybrid", "hybrid", "splondee", null, "hybrid", null, "splondee", null, null, "splondee", null]
-// };
-
 var level9 = {
 	drinks: ["liquor", "wine", null, "soda", "tequila"],
 	pipes: [
-		[["rect", 0], ["curve", 1], ["rect", 0], ["bridge", 0], ["rect", 0], ["mixer", 0], ["rect", 0], ["cross", 0], ["rect", 0], ["rect", 1], ["rect", 0]],
-		[["segundo", 0], ["curve", 1], ["rect", 0], ["curve", 1], ["mixer", 0], ["bridge", 0], ["rect", 0], ["segundo", 0], ["rect", 0], ["rect", 1], ["curve", 0]],
-		[["curve", 0], ["curve", 2], ["rect", 0], ["rect", 0], ["segundo", 0], ["rect", 1], ["rect", 0], ["curve", 2], ["rect", 0], ["curve", 0], ["rect", 0]],
-		[["curve", 1], ["rect", 1], ["segundo", 0], ["rect", 0], ["rect", 0], ["segundo", 1], ["mixer", 0], ["segundo", 0], ["segundo", 0], ["curve", 0], ["curve", 1]],
-		[["curve", 2], ["cross", 0], ["bridge", 0], ["rect", 0], ["curve", 3], ["curve", 1], ["cross", 0], ["rect", 0], ["rect", 0], ["curve", 3], ["curve", 3]]
+		[["rect", 0], ["rect", 1], ["curve", 2], ["curve", 2], ["curve", 3], ["mixer", 0], ["cross", 0], ["rect", 0], ["rect", 1], ["rect", 0], ["segundo", 1]],
+		[["rect", 0], ["cross", 0], ["bridge", 0], ["curve", 1], ["mixer", 0], ["curve", 3], ["rect", 1], ["segundo", 0], ["curve", 1], ["curve", 1], ["rect", 0]],
+		[["rect", 1], ["rect", 0], ["rect", 0], ["rect", 0], ["rect", 0], ["curve", 3], ["curve", 1], ["curve", 0], ["bridge", 0], ["rect", 0], ["curve", 2]],
+		[["rect", 0], ["curve", 0], ["curve", 0], ["cross", 0], ["segundo", 0], ["curve", 0], ["mixer", 0], ["rect", 0], ["rect", 0], ["rect", 0], ["rect", 0]],
+		[["segundo", 0], ["rect", 0], ["segundo", 0], ["rect", 0], ["segundo", 0], ["curve", 1], ["segundo", 0], ["bridge", 0], ["rect", 0], ["rect", 0], ["curve", 1]]
 	],
 	receiver: ["hybrid", "hybrid", "splondee", null, "hybrid", null, "splondee", null, null, "splondee", null]
 };
+
+// var level9 = {
+// 	drinks: ["liquor", "wine", null, "soda", "tequila"],
+// 	pipes: [
+// 		[["rect", 0], ["curve", 1], ["rect", 0], ["bridge", 0], ["rect", 0], ["mixer", 0], ["rect", 0], ["cross", 0], ["rect", 0], ["rect", 1], ["rect", 0]],
+// 		[["segundo", 0], ["curve", 1], ["rect", 0], ["curve", 1], ["mixer", 0], ["bridge", 0], ["rect", 0], ["segundo", 0], ["rect", 0], ["rect", 1], ["curve", 0]],
+// 		[["curve", 0], ["curve", 2], ["rect", 0], ["rect", 0], ["segundo", 0], ["rect", 1], ["rect", 0], ["curve", 2], ["rect", 0], ["curve", 0], ["rect", 0]],
+// 		[["curve", 1], ["rect", 1], ["segundo", 0], ["rect", 0], ["rect", 0], ["segundo", 1], ["mixer", 0], ["segundo", 0], ["segundo", 0], ["curve", 0], ["curve", 1]],
+// 		[["curve", 2], ["cross", 0], ["bridge", 0], ["rect", 0], ["curve", 3], ["curve", 1], ["cross", 0], ["rect", 0], ["rect", 0], ["curve", 3], ["curve", 3]]
+// 	],
+// 	receiver: ["hybrid", "hybrid", "splondee", null, "hybrid", null, "splondee", null, null, "splondee", null]
+// };
 
 
